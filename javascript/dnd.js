0946af43fe61b2c4b3b997f699b77ab8803a7536
Paul <pjj007@student.usc.edu.au>
2017-06-12 14:58:45 +1000

    first commit, new repository

--- javascript/dnd.js
+++ javascript/dnd.js
@@ -1,5 +1,7 @@
 function allowDrop(ev) {
+  console.log("allow drop", ev);
     ev.preventDefault();
+
 }
 
 function drag(ev) {
@@ -7,19 +9,30 @@ function drag(ev) {
 }
 
 function drop(ev) {
+    ev.stopPropagation();
     ev.preventDefault();
+    console.log("drop", ev);
     //var data = ev.dataTransfer.getData("text");
-    var span = ev.dataTransfer.getData("text");
+    var data = ev.dataTransfer.getData("text");
     while (ev.target.firstChild) {
-    ev.target.removeChild(ev.target.firstChild);
+      ev.target.removeChild(ev.target.firstChild);
     }
-    //var img = document.getElementById(data);
-    var list1 = document.getElementById('list');
-    console.log("ev:", ev, "img:", list1, "from:", ev.dataTransfer.dragStartedFrom);
-    list1.width = 150;
-    list1.height = 100;
-    //ev.target.appendChild(img);
-    ev.target.appendChild(list1);
+
+    var img = document.getElementById(data);
+    console.log("image data", data, ev.target, "ev:", ev, "img:", img, "from:", ev.dataTransfer.dragStartedFrom);
+    if(img != null) ev.target.appendChild(img);
+
+    // if (img == null) {
+    //     img.appendChild(img);
+    // } else console.log("aaaaa");
+    // var list1 = document.getElementById('list');
+    // console.log(data, ev.target, "ev:", ev, "img:", img, "from:", ev.dataTransfer.dragStartedFrom);
+    // list1.width = 150;
+    // list1.height = 100;
+
+
+    // document.getElementById(data).appendChild(img);
+    // ev.target.appendChild(img);
 }
 
 function dragstart(ev) {
--- javascript/upload.js
+++ javascript/upload.js
@@ -35,18 +35,19 @@ function upload_image(img_name, img_data) {
   });
 }
 
-
 function handleFileSelect(ev) {
   var files = ev.target.files; // FileList object
+  var output = document.getElementById("frames");
+  var filesCount = [];
 
   // Loop through the FileList and render image files as thumbnails.
-  for (var i = 0, f; f = files[i]; i++) {
-
-    let file = files[i];
+    for (var i = 0, f; f = files[i]; i++) {
+      filesCount.push(i);
+      let file = files[i];
     // Only process image files.
-    if (!f.type.match('image.*')) {
-      continue;
-    }
+        if (!f.type.match('image.*')) {
+          continue;
+        }
 
     var reader = new FileReader();
 
@@ -54,17 +55,78 @@ function handleFileSelect(ev) {
     reader.onload = (function(theFile) {
       return function(e) {
         // Render thumbnail.
+        console.log("reader onload", ev);
         var span = document.createElement('span');
-        span.innerHTML = ['<img class="thumb" src="', e.target.result,
+        var item = document.createElement('li');
+        var palette = document.getElementById('palette');
+        item.innerHTML = ['<img class="thumb" src="', e.target.result,
                           '" title="', escape(theFile.name), '"/>'].join('');
-        document.getElementById('list').insertBefore(span, null);
-        upload_image(file.name, file);
+        if (palette != null) {
+          palette.appendChild(item);
+        } else console.log("failed to append");
+
+        current_i = filesCount.shift();
+          if(current_i === 0) {
+            prev_i = files.length - 1;
+          } else {
+            prev_i = current_i - 1;
+          }
+          if(filesCount.length === 0) {
+            next_i = 0;
+          } else {
+            next_i = current_i + 1;
+          }
+
+
+          // output.innerHTML = output.innerHTML + '<li id="slide-' + current_i +
+          // '" class="slide">' + "<img src='" + files.result + "'" + "title=''/>"
+          // + '<nav>' + '<a class="prev" href="#slide-' + prev_i + '">&larr;</a>'
+          // + '<a class="next" href="#slide-' + next_i + '">&rarr;</a>'
+          // + '</nav>' + '<li>';
+
+
+
+
+
+        for ( var i = ev ; i < ev ; i++ ) {
+        var img = document.createElement('img');
+        if (i < 10) {
+          img.setAttribute("src", "0" + i + ".jpg");
+        } else {
+          img.setAttribute("src", i + ".jpg");
+        }
+        console.log("img tag", img);
+        img.class = "thumb";
+        img.src = e.target.result;
+        img.title = escape(theFile.name);
+        img.draggable = true;
+        img.width = '100px';
+        img.ondragover = 'allowDrop(event)';
+        img.id = escape(theFile.name);
+        img.id != 0;
+        img.id = "abc";
+        item.appendChild(img);
+        palette.appendChild(item);
+      }
+        // document.getElementById('list').insertBefore(span, null);
+        document.getElementById('subbtn').onclick = function(){
+          upload_image(file.name, file)};
+        // upload_image(file.name, file);
       };
     })(f);
 
     // Read in the image file as a data URL.
     reader.readAsDataURL(f);
+      // document.getElementById('subbtn').onclick = upload_image(file.name, file);
+
+      function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
   }
+
+  }
+
 }
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
